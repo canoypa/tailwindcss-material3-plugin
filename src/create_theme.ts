90e3f940505219ec316ac93e4ea64ffe0b654a3b
Cano <developer.sasazono@gmail.com>
2023-04-26 16:34:36 +0900

    create theme fn
    make custom colors good mood

---
+++ src/create_theme.ts
@@ -0,0 +1,48 @@
+import {
+  CorePalette,
+  TonalPalette,
+  themeFromSourceColor,
+} from "@material/material-color-utilities";
+import { Material3PluginOptions } from ".";
+
+export interface Theme {
+  schemes: {
+    light: Record<string, number>;
+    dark: Record<string, number>;
+  };
+  palettes: Record<string, TonalPalette>;
+}
+
+export function createTheme({
+  sourceColor,
+  customColors,
+}: Material3PluginOptions): Theme {
+  const theme = themeFromSourceColor(sourceColor, customColors);
+
+  const schemeLight: Record<string, number> = theme.schemes.light.toJSON();
+  const schemeDark: Record<string, number> = theme.schemes.dark.toJSON();
+
+  const palettes: Record<string, TonalPalette> = theme.palettes;
+
+  theme.customColors.forEach((c) => {
+    schemeLight[c.color.name] = c.light.color;
+    schemeLight[`on-${c.color.name}`] = c.light.onColor;
+    schemeLight[`${c.color.name}-container`] = c.light.colorContainer;
+    schemeLight[`on-${c.color.name}-container`] = c.light.onColorContainer;
+
+    schemeDark[c.color.name] = c.dark.color;
+    schemeDark[`on-${c.color.name}`] = c.dark.onColor;
+    schemeDark[`${c.color.name}-container`] = c.dark.colorContainer;
+    schemeDark[`on-${c.color.name}-container`] = c.dark.onColorContainer;
+
+    palettes[c.color.name] = CorePalette.of(c.value).a1;
+  });
+
+  return {
+    schemes: {
+      light: schemeLight,
+      dark: schemeDark,
+    },
+    palettes,
+  };
+}
