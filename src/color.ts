68008aaa9ed54b43422b2fe2915ca29a06b0ac60
Cano <developer.sasazono@gmail.com>
2023-04-27 11:13:51 +0900

    adjust color names

--- src/color.ts
+++ src/color.ts
@@ -2,11 +2,11 @@ import { hexFromArgb, TonalPalette } from "@material/material-color-utilities";
 import { Theme } from "./create_theme";
 import { kebab } from "./kebab_case";
 
-const schemeColors = (scheme: Record<string, number>) => {
+const schemeColors = (scheme: Record<string, number>, mode: string) => {
   const result: Record<string, string> = {};
 
   for (const key in scheme) {
-    result[kebab(key)] = hexFromArgb(scheme[key]);
+    result[`${kebab(key)}-${mode}`] = hexFromArgb(scheme[key]);
   }
 
   return result;
@@ -14,12 +14,11 @@ const schemeColors = (scheme: Record<string, number>) => {
 
 const tones = [0, 10, 20, 25, 30, 35, 40, 50, 60, 70, 80, 90, 95, 98, 99, 100];
 const paletteColors = (palette: Record<string, TonalPalette>) => {
-  const result: Record<string, Record<string, string>> = {};
+  const result: Record<string, string> = {};
 
   for (const key in palette) {
-    result[key] = {};
     tones.forEach((tone) => {
-      result[key][tone] = hexFromArgb(palette[key].tone(tone));
+      result[`${key}-${tone}`] = hexFromArgb(palette[key].tone(tone));
     });
   }
 
@@ -28,8 +27,8 @@ const paletteColors = (palette: Record<string, TonalPalette>) => {
 
 export const colors = (m3Theme: Theme) => {
   return {
-    light: schemeColors(m3Theme.schemes.light),
-    dark: schemeColors(m3Theme.schemes.dark),
-    palette: paletteColors(m3Theme.palettes),
+    ...schemeColors(m3Theme.schemes.light, "light"),
+    ...schemeColors(m3Theme.schemes.dark, "dark"),
+    ...paletteColors(m3Theme.palettes),
   };
 };
